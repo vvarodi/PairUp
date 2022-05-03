55eb4bc805187965db7d7e28c2002b336caa68a7
vvarodi <vikivv2000@gmail.com>
2022-05-03 22:20:21 +0200

    change text when location set and botton buttons maps

--- app/src/main/java/com/example/pairup/MapsActivity.java
+++ app/src/main/java/com/example/pairup/MapsActivity.java
@@ -1,8 +1,16 @@
 package com.example.pairup;
 
+import android.annotation.SuppressLint;
+import android.content.Context;
+import android.content.pm.PackageManager;
+import android.location.Location;
+import android.location.LocationListener;
+import android.location.LocationManager;
 import android.os.Bundle;
+import android.widget.TextView;
 import android.widget.Toast;
 
+import androidx.annotation.NonNull;
 import androidx.appcompat.app.AppCompatActivity;
 
 import com.google.android.gms.maps.CameraUpdateFactory;
@@ -10,15 +18,21 @@ import com.google.android.gms.maps.GoogleMap;
 import com.google.android.gms.maps.OnMapReadyCallback;
 import com.google.android.gms.maps.SupportMapFragment;
 import com.google.android.gms.maps.model.LatLng;
+import com.google.android.gms.maps.model.MarkerOptions;
 import com.google.android.gms.maps.model.PointOfInterest;
 
 
 public class MapsActivity extends AppCompatActivity implements OnMapReadyCallback, GoogleMap.OnPoiClickListener {
 
+    private TextView text_clicked;
+
+    @SuppressLint("MissingPermission")
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_maps);
+
+        text_clicked = (TextView)findViewById(R.id.text_location);
 
         SupportMapFragment mapFragment = (SupportMapFragment) getSupportFragmentManager()
                 .findFragmentById(R.id.map);
@@ -31,13 +45,20 @@ public class MapsActivity extends AppCompatActivity implements OnMapReadyCallbac
 
         float zoomLevel = map.getMaxZoomLevel() - 5;
         LatLng uc3m = new LatLng(40.332690215919904, -3.765110001822394); // UC3M Leganés
+
+        map.addMarker(new MarkerOptions().position(uc3m));
         map.moveCamera(CameraUpdateFactory.newLatLngZoom(uc3m, zoomLevel));
     }
 
     @Override
     public void onPoiClick(PointOfInterest poi) {
+
+        //update();
         Toast.makeText(this, "Clicked: " + poi.name + "\nPlace ID:" + poi.placeId + "\nLatitude:" +
                 poi.latLng.latitude + " Longitude:" + poi.latLng.longitude, Toast.LENGTH_SHORT)
                 .show();
+
+        text_clicked.setText(poi.name);
     }
+
 }
